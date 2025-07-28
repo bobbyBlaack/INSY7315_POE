95d8506403005ed0d11e1a882e10475451557c27
LoyisoMotsamai <128675393+LoyisoMotsamai@users.noreply.github.com>
2025-07-28 10:53:24 +0200

    Initial web app push

---
+++ POE_part1_Version1/POE_part1_Version1/Controllers/PropManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace POE_part1_Version1.Controllers
+{
+    public class PropManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ POE_part1_Version1/POE_part1_Version1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POE_part1_Version1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
