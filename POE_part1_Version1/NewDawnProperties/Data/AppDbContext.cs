bca95291093ce0cd910549e8a0ca1db18c6a655b
LoyisoMotsamai <st10021729@vcconnect.edu.za>
2025-09-16 15:07:22 +0200

    making models and the dbcontext
    as the name suggests, also I added some nuget packages

--- POE_part1_Version1/NewDawnProperties/Data/AppDbContext.cs
+++ POE_part1_Version1/NewDawnProperties/Data/AppDbContext.cs
@@ -1,9 +1,26 @@
-﻿namespace NewDawnProperties.Data
+﻿using NewDawnProperties.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace NewDawnProperties.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
 
-        int snake = 124;
+        public DbSet <CaretakerAssignmentModel> CaretakerAssignment {  get; set; }
+
+        public DbSet<PropertyModel> Property { get; set; }
+
+        public DbSet<RoomModel> Rooms { get; set; }
+
+        public DbSet<TenantAssignmentModel> TenantAssignment { get; set; }
+
+        public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
 
     }
 }
